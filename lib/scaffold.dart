6b2ea2e1669f618f779174fff7a512086d0297b8
Lincxx <bossco28@gmail.com>
2019-04-03 06:16:07 -0400

    Added in a chat view

--- lib/scaffold.dart
+++ lib/scaffold.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/chat_head.dart';
+import 'package:chat_app/chat_view.dart';
 import 'package:flutter/material.dart';
 
 class MyChatApp extends StatefulWidget {
@@ -11,55 +12,57 @@ class _MyChatAppState extends State<MyChatApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My Chat"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.help),
-              onPressed: () {
-                print('button was pressed');
-              },
-            )
-          ],
-        ),
-        body: ListView(
-          children: <Widget>[
-            ChatHead(),
-            ChatHead(),
-            ChatHead(),
-          ],
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: Text('Home'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.mail_outline),
-              title: Text('Chats'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              title: Text('Settings'),
-            ),
-          ],
-          onTap: (int index) {
-            print('index is $index');
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('FAB was pressed');
-          },
-          child: Icon(Icons.help),
-        ),
-      ),
-    );
+        // home: Scaffold(
+        //   appBar: AppBar(
+        //     title: Text("My Chat"),
+        //     actions: <Widget>[
+        //       IconButton(
+        //         icon: Icon(Icons.help),
+        //         onPressed: () {
+        //           print('button was pressed');
+        //         },
+        //       )
+        //     ],
+        //   ),
+        //   body: ListView(
+        //     children: <Widget>[
+        //       ChatHead(),
+        //       ChatHead(),
+        //       ChatHead(),
+        //     ],
+        //   ),
+        //   bottomNavigationBar: BottomNavigationBar(
+        //     currentIndex: _currentIndex,
+        //     items: <BottomNavigationBarItem>[
+        //       BottomNavigationBarItem(
+        //         icon: Icon(Icons.home),
+        //         title: Text('Home'),
+        //       ),
+        //       BottomNavigationBarItem(
+        //         icon: Icon(Icons.mail_outline),
+        //         title: Text('Chats'),
+        //       ),
+        //       BottomNavigationBarItem(
+        //         icon: Icon(Icons.settings),
+        //         title: Text('Settings'),
+        //       ),
+        //     ],
+        //     onTap: (int index) {
+        //       print('index is $index');
+        //       setState(() {
+        //         _currentIndex = index;
+        //       });
+        //     },
+        //   ),
+
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {
+        //     print('FAB was pressed');
+        //   },
+        //   child: Icon(Icons.help),
+        // ),
+
+        //),
+        home: ChatView());
   }
 }
